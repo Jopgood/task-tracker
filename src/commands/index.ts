0b1d0ff9e73656464975de83660b35ee7e5e4d8c
Jopgood <97100755+Jopgood@users.noreply.github.com>
2024-09-28 08:58:25 +0000

    New filtering on task-cli list
    NEW
    ==
    + Now when you call on task-cli list you can optionally add a [status]
        + options are todo, in-progress, done

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,5 @@
 export { default as add } from "./add-task";
-export { default as list } from "./list-tasks";
+export { listAll } from "./list-tasks";
 export { default as update } from "./update-task";
 export { default as delete } from "./delete-task";
-export {markDoneCmd, markInProgressCmd} from "./update-task-status"
+export { markDoneCmd, markInProgressCmd } from "./update-task-status";
